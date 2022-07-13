f9e2df1878c46b52ce4269622cb6d5f5b2fd1a6c
ncyu <as9909022@gmail.com>
2022-07-13 12:32:48 +0800

    Fix commit

--- src/test/java/GradeConverterTest.java
+++ src/test/java/GradeConverterTest.java
@@ -2,6 +2,10 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class GradeConverterTest {
 
